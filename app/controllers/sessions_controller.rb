9e31a1fff7f152f9b38e59bbfcc6b77107d3bc24
dwanerich <dwanerich@gmail.com>
2020-06-04 00:04:51 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,15 @@
 class SessionsController < ApplicationController
 
     def new
-        render :'/users/new'
     end
 
     def create
         @user = User.find_by(name: params[:name])
         return head(:forbidden) unless @user.authenticate(params[:password])
         session[:user_id] = @user.id
-        redirect_to user_path(@user)
     end
 
     def destroy
-        session.delete
+        session.delete :name
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,12 @@ class UsersController < ApplicationController
 
     def create
             @user = User.create(user_params)
+            if !@user.authenticate(params[:password])
+                render :new
+            end
             return head(:forbidden) unless @user.authenticate(params[:password])
             session[:user_id] = @user.id
             redirect_to user_path(@user)
-
-        if !@user.authenticate(params[:password])
-            render :login
-        end
     end
 
 
